12261be6fe9b83e155df051654c2c8b738f96aaa
git-leon <xleonhunter@gmail.com>
2020-06-10 15:42:09 -0400

    first commit

---
+++ src/main/java/com/github/curriculeon/Board.java
@@ -0,0 +1,26 @@
+package com.github.curriculeon;
+
+/**
+ * @author leon on 6/22/18.
+ */
+public class Board {
+    public Board(Character[][] matrix) {
+    }
+
+    public Boolean isInFavorOfX() {
+        return null;
+    }
+
+    public Boolean isInFavorOfO() {
+        return null;
+    }
+
+    public Boolean isTie() {
+        return null;
+    }
+
+    public String getWinner() {
+        return null;
+    }
+
+}

c3db8e3e0a750936c62eadc91bbf97d20a20f863
suresh kandagatla <surikandagatla@gmail.com>
2020-07-16 16:33:35 -0400

    Finished exercise Not so perfect

--- src/main/java/com/github/perscholas/Board.java
+++ src/main/java/com/github/perscholas/Board.java
@@ -1,26 +1,59 @@
 package com.github.perscholas;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private Character[][] game;
+
     public Board(Character[][] matrix) {
+        this.game = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        return isInFavorOfXO('X');
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+
+        return isInFavorOfXO('O');
     }
 
     public Boolean isTie() {
-        return null;
+            return !isInFavorOfX() && !isInFavorOfO();
     }
 
     public String getWinner() {
-        return null;
+        if(isInFavorOfX()){
+            return "X";
+        } else if(isInFavorOfO()) {
+            return "O";
+        }
+        return "";
+    }
+
+    public Boolean isInFavorOfXO(char XO){
+        if((game[0][0]==XO) && (game[0][1] == XO) && (game[0][2] == XO)){
+            return true;
+        } else if((game[1][0]==XO) && (game[1][1]==XO) && (game[1][2]== XO)){
+            return true;
+        } else if((game[2][0]==XO) && (game[2][1]==XO) && (game[2][2]==XO)){
+            return true;
+        } else if((game[0][0]==XO) && (game[0][1]==XO) && (game[0][2]==XO)){
+            return true;
+        } else if((game[1][0]==XO) && (game[1][1]==XO) && (game[1][2]==XO)){
+            return true;
+        } else if((game[2][0]==XO) && (game[2][1]==XO) && game[2][2]==XO){
+            return true;
+        } else if((game[0][0]==XO) && (game[1][1]==XO) && (game[2][2]==XO)){
+            return true;
+        } else if((game[0][2]==XO) && (game[1][1]==XO) && (game[2][0]==XO)){
+            return true;
+        } return false;
     }
 
 }
